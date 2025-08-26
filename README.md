d4972b802f3f902557d99f147361248bd6175b7c
Joia <gustavo.sc.joia@gmail.com>
2025-08-26 01:59:46 -0300

    INIT: Inicialização do projeto

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Esfera Odontologia & Saúde
+
+## Rodar o projeto em ambiente local
+1. Preparar ambiente:
+```sh
+python -m venv venv
+.\venv\Scripts\activate
+pip install fastapi uvicorn jinja2
+```
+
+2. Rodar e atualizar automaticamente a cada mudança:
+
+```sh
+uvicorn app:app --reload
+```
---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Configurar a pasta de templates
+templates = Jinja2Templates(directory="templates")
+
+# Rota principal
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
